23bb367dad44cc8b5538214ee8a6867bb8e63ec8
zoey <1291609705@qq.com>
2020-07-02 15:51:41 +0800

    ng-electron打包

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgxElectronModule } from 'ngx-electron';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxElectronModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
